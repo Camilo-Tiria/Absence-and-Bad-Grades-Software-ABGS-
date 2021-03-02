005cfa6c1f0bc8f90b51d167757ecbf419cbc512
leidycalderon <79711253+leidycalderon@users.noreply.github.com>
2021-03-02 15:42:32 -0500

    Add files via upload

---
+++ Carpeta/Código/Vista/piePagina.php
@@ -0,0 +1,5 @@
+<footer width="100%" border="0" align="center">
+  <tr>
+    <marquee><font face="Brush Script MT" size="300"> **ABSENCE AND BAD GRADES SOFTWARE**</font></marquee>
+  </tr>
+</fooster>
